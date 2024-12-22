a3414a322b0753682c48b520478dd50a3afcf5c7
kristinap1001 <kristinap1001@gmail.com>
2024-12-22 16:42:11 -0500

    More code/data cleanup

--- hhpetl.py
+++ hhpetl.py
@@ -13,11 +13,6 @@ pd.set_option('display.max_rows', 0)
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.width', 0)
 
-# Count number of acnh csvs and rename files
-dataDir = 'acnh_data'
-dataFiles = len([name for name in os.listdir(dataDir) if os.path.isfile(os.path.join(dataDir, name))])
-print(dataFiles)
-
 # Rename csvs (only need to do once)
 for filename in os.listdir('acnh_data'):
 	if filename.startswith('Data Spreadsheet'):
@@ -25,14 +20,14 @@ for filename in os.listdir('acnh_data'):
 	else:
 		newName = filename
 	newName = newName.lower().replace(" ","")
-	os.rename(dataDir+'/'+filename, dataDir+'/'+newName)
+	os.rename('acnh_data/'+filename, 'acnh_data/'+newName)
 
 # Create dataframes
 varList = []
 dfList = []
-for filename in os.listdir(dataDir):
+for filename in os.listdir('acnh_data'):
 	varName = os.path.splitext(filename)[0] # removes '.csv'
-	data = pd.read_csv(dataDir+'/'+filename,dtype=object)
+	data = pd.read_csv('acnh_data/'+filename,dtype=object)
 	globals()[varName] = data
 
 	# Add column with the category/"tab" name
@@ -43,8 +38,8 @@ for filename in os.listdir(dataDir):
 	dfList.append(globals()[varName])
 
 # Import HHP furniture lists
-villagerUnlocks = pd.read_csv('furniture-per-villager-h.csv',dtype=object)
-facilityUnlocks = pd.read_csv('furniture-per-facility-v.csv',dtype=object)
+villagerUnlocks = pd.read_csv('hhp_data/furniture-per-villager.csv',dtype=object)
+facilityUnlocks = pd.read_csv('hhp_data/furniture-per-facility.csv',dtype=object)
 
 # typo in facilities csv
 facilityUnlocks.loc[facilityUnlocks['English Name (added)'] == "tee with silicon bib", 'English Name (added)'] = "tee with silicone bib"
@@ -55,7 +50,7 @@ villToFurn = {
 	for _, row in villagerUnlocks.iterrows()
 }
 
-# the facilities csv is so fucking messy
+# wrangling the messy facilities csv
 facilities = ["School", "Cafe", "Restaurant", "Hospital", "Apparel Shop", "Start"]
 for facility in facilities:
     villToFurn[facility] = []
@@ -99,7 +94,7 @@ for vill,furnList in villToFurn.items():
 
 # Create dataframe with all items
 megaDf = pd.concat(dfList,ignore_index=True)
-megaDf.to_csv("output/mega.csv")
+#megaDf.to_csv("output/mega.csv")
 
 # Customize/Cyrus customize booleans
 megaDf['Customize'] = megaDf['Kit Cost'].notnull()
@@ -262,11 +257,7 @@ decorCedar = {
 decorModeItems = pd.DataFrame([wallpaperItem,flooringItem,recipeItem,rock,hardwoodStump,cedarStump,coconutStump,bambooStump,decorCedar])
 megaDf = pd.concat([megaDf,decorModeItems],ignore_index=True)
 
-# Check if any furniture items exist in dict but not in df
-#missing = [i for i in furnToVill if i not in megaDf['Name'].tolist() and "'s poster" not in i and "'s photo" not in i]
-#print(missing)
-
-# Fix missing entries
+# Fix missing or misspelled entries
 megaDf.loc[megaDf['Name'] == 'welding mask', 'Name'] = 'welding mask (Red)'
 megaDf.loc[megaDf['Name'] == 'matronly bun', 'Name'] = 'matronly bun (Hair Color)'
 megaDf.loc[megaDf['Name'] == 'Noh mask', 'Name'] = 'Noh mask (White)'
@@ -296,103 +287,18 @@ megaDf.loc[megaDf['Name'] == "stagehand hat", 'Name'] = "stagehand hat (Black)"
 megaDf.loc[megaDf['Name'] == "handlebar mustache", 'Name'] = "handlebar mustache (Hair Color)"
 
 # Missing values are bivalve/scallop (in sea creatures table, not included) and Sanrio posters (photos and posters tables not included)
-missing = [i for i in furnToVill if i not in megaDf['Name'].tolist() and "'s poster" not in i and "'s photo" not in i]
-print(missing)
+#missing = [i for i in furnToVill if i not in megaDf['Name'].tolist() and "'s poster" not in i and "'s photo" not in i]
+#print(missing)
+
+# Fixing null/inconsistent column values/types
+megaDf['Catalog'].fillna("Not in catalog")
+megaDf['DIY'] = megaDf['DIY'].map({'Yes':True,'No':False}).astype(bool)
 
 # Add list of villagers/facilities to each item
 megaDf['HHP Source'] = megaDf['Name'].map(furnToVill)
+
+# todo: Fill in HHP sources aside from villagers/facilities
+
 print(megaDf)
-
-megaDf.to_csv("output/mega_nobs.csv")
-
-
-'''
- #   Column                     Non-Null Count  Dtype
----  ------                     --------------  -----
- 0   Name                       20097 non-null  object
- 1   Closet Image               5380 non-null   object x
- 2   Storage Image              5756 non-null   object x
- 3   Variation                  17153 non-null  object
- 4   DIY                        19917 non-null  object
- 5   Buy                        20097 non-null  object
- 6   Sell                       19962 non-null  object
- 7   HHA Base Points            19998 non-null  object x
- 8   Color 1                    19626 non-null  object x
- 9   Color 2                    19622 non-null  object x
- 10  Size                       19073 non-null  object x
- 11  Exchange Price             12714 non-null  object x
- 12  Exchange Currency          12714 non-null  object x
- 13  Source                     20097 non-null  object
- 14  Source Notes               7968 non-null   object
- 15  Season/Event               2365 non-null   object x
- 16  Season/Event Exclusive     2361 non-null   object x
- 17  Seasonal Availability      5432 non-null   object x
- 18  Seasonality                5432 non-null   object x
- 19  Mannequin Season           454 non-null    object x
- 20  Gender                     5500 non-null   object x
- 21  Villager Gender            3607 non-null   object x
- 22  Style 1                    5432 non-null   object x
- 23  Style 2                    5431 non-null   object x
- 24  Sort Order                 5432 non-null   object x
- 25  Label Themes               5432 non-null   object x
- 26  Type                       1169 non-null   object x
- 27  Villager Equippable        5814 non-null   object x
- 28  Catalog                    19687 non-null  object
- 29  Version Added              20097 non-null  object x
- 30  Unlocked?                  20097 non-null  object x
- 31  Filename                   19687 non-null  object x
- 32  ClothGroup ID              5432 non-null   object x
- 33  Internal ID                20097 non-null  object x
- 34  Unique Entry ID            20097 non-null  object
- 35  Tab                        20097 non-null  object
- 36  Image                      14077 non-null  object x
- 37  High-Res Texture           46 non-null     object x
- 38  Genuine                    70 non-null     object x
- 39  Category                   70 non-null     object x
- 40  Real Artwork Title         70 non-null     object x
- 41  Artist                     70 non-null     object x
- 42  Description                70 non-null     object x
- 43  HHA Concept 1              13471 non-null  object x
- 44  HHA Concept 2              10162 non-null  object x
- 45  HHA Series                 3831 non-null   object x
- 46  HHA Set                    569 non-null    object x
- 47  Interact                   12940 non-null  object x
- 48  Tag                        14074 non-null  object
- 49  Speaker Type               11790 non-null  object x
- 50  Lighting Type              13254 non-null  object x
- 51  Body Title                 12113 non-null  object x
- 52  Pattern                    7063 non-null   object x
- 53  Pattern Title              7479 non-null   object x
- 54  Body Customize             12870 non-null  object x
- 55  Pattern Customize          12870 non-null  object x
- 56  Pattern Customize Options  7479 non-null   object x
- 57  Kit Cost                   9279 non-null   object x
- 58  Kit Type                   9279 non-null   object x
- 59  Cyrus Customize Price      12382 non-null  object x
- 60  HHA Category               2638 non-null   object x
- 61  Outdoor                    12870 non-null  object x
- 62  Variant ID                 12382 non-null  object x
- 63  Primary Shape              1069 non-null   object x
- 64  Secondary Shape            1069 non-null   object x
- 65  Customize                  413 non-null    object
- 66  Stack Size                 4006 non-null   object x
- 67  VFX                        515 non-null    object x
- 68  Surface                    11720 non-null  object x
- 69  Food Power                 271 non-null    object x
- 70  Framed Image               107 non-null    object x
- 71  Album Image                107 non-null    object x
- 72  Inventory Image            404 non-null    object x
- 73  Inventory Filename         410 non-null    object X
- 74  Storage Filename           410 non-null    object X
- 75  Size Category              209 non-null    object X
- 76  Uses                       314 non-null    object X
- 77  Set                        80 non-null     object X
- 78  Door Deco                  731 non-null    object X
- 79  VFX Type                   38 non-null     object X
- 80  Window Type                151 non-null    object X
- 81  Window Color               151 non-null    object X
- 82  Pane Type                  151 non-null    object X
- 83  Curtain Type               99 non-null     object X
- 84  Curtain Color              99 non-null     object X
- 85  Ceiling Type               306 non-null    object X
- '''
+megaDf.info()
+megaDf.to_csv("output/allitems.csv")
